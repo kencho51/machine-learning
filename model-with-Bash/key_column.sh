fe2bb6491a6412a182904512d0d5a6a499c2af59
Ken <kencho.gigascience@gmail.com>
2020-12-09 16:36:42 +0800

    Add new scripts

---
+++ model-with-Bash/key_column.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# For each file with name that starts with "TAVG_US_" and ens with ".csv"
+for csv_file in `ls /processed-data/TAVG_US_*.csv`
+do
+    echo "Exracting columns $csv_file"
+    # cat $csv_file: 'cat' is to concatenate files - here used to show one year csv file
+    # |: pipe
+    # cut -d',' -f1,2,4: Cut columns 1,2,4 with , delimitor
+    # > $csv_file.cut: Save to temporary file
+    # > $csv_file.cut:
+    cat $csv_file | cut -d',' -f1,2,4 > /processed-data/$csv_file.cut
+    # mv $csv_file.cut $csv_file: Rename temporary file to original file
+    mv /processed-data/$csv_file.cut /processed-data/$csv_file
+    # File is processed and saved back into the same
+    # There are other ways to do this
+    # Using intermediate file is the most reliable method.
+done
