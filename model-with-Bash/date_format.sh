b771e7756beced105a552b3ffa91843a86ca7fae
Ken <kencho.gigascience@gmail.com>
2020-12-10 15:57:33 +0800

    Add date bash script

---
+++ model-with-Bash/date_format.sh
@@ -0,0 +1,10 @@
+cd processed-data
+for csv_file in `ls TAVG_*.csv`
+do
+    echo Date formatting $csv_file
+    # This inserts , after year
+    # in Mac, -i edits files in-place
+    sed -i '' 's/,..../&,/' $csv_file
+    # This inserts , after month
+    sed -i '' 's/,....,../&,/' $csv_file
+done
