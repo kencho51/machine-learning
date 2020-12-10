adb58350a3dcd944a6e8a1f2fe52640ff6c8fb71
Ken <kencho.gigascience@gmail.com>
2020-12-10 15:38:25 +0800

    To make sure the file path work

--- model-with-Bash/extract_tavg_us.sh
+++ model-with-Bash/extract_tavg_us.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 # For each file with name that starts with "20" and ens with ".csv"
-for csv_file in `ls raw-data/20*.csv`
+cd raw-data
+for csv_file in `ls 20*.csv`
 do
     # Message that says file name $csv_file is extracted to file TAVG_US_$csv_file
     # Example: 2010.csv extracted to TAVG_US_2010.csv
@@ -10,5 +11,5 @@ do
     # |: pipe
     # grep "^US": From those extract lines that begin with text "US"
     # > TAVG_US_$csv_file: Save xtracted lines to file TAVG_US_$csv_file
-    grep "TAVG" $csv_file | grep "^US" > TAVG_US_$csv_file
+    grep "TAVG" $csv_file | grep "^US" > /Volumes/kencho/machine-learning/model-with-Bash/processed-data/"TAVG_US_"$csv_file
 done
