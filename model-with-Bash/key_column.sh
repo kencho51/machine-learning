59613168a3805f8a4bdcfae181cb354d0f6aa8f8
Ken <kencho.gigascience@gmail.com>
2020-12-10 15:44:14 +0800

    Provide exact path

--- model-with-Bash/key_column.sh
+++ model-with-Bash/key_column.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 # For each file with name that starts with "TAVG_US_" and ens with ".csv"
-for csv_file in `ls /processed-data/TAVG_US_*.csv`
+cd processed-data
+for csv_file in `ls TAVG_US_*.csv`
 do
     echo "Exracting columns $csv_file"
     # cat $csv_file: 'cat' is to concatenate files - here used to show one year csv file
@@ -9,9 +10,9 @@ do
     # cut -d',' -f1,2,4: Cut columns 1,2,4 with , delimitor
     # > $csv_file.cut: Save to temporary file
     # > $csv_file.cut:
-    cat $csv_file | cut -d',' -f1,2,4 > /processed-data/$csv_file.cut
+    cat $csv_file | cut -d',' -f1,2,4 > /Volumes/kencho/machine-learning/model-with-Bash/processed-data/$csv_file.cut
     # mv $csv_file.cut $csv_file: Rename temporary file to original file
-    mv /processed-data/$csv_file.cut /processed-data/$csv_file
+    mv /Volumes/kencho/machine-learning/model-with-Bash/processed-data/$csv_file.cut /Volumes/kencho/machine-learning/model-with-Bash/processed-data/$csv_file
     # File is processed and saved back into the same
     # There are other ways to do this
     # Using intermediate file is the most reliable method.
